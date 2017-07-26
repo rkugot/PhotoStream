e505179f2b24c92780dda122c0edae6394f1b4be
rkugot <stream13k@gmail.com>
2017-07-26 17:52:17 +0300

    Add delete action and some refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -22,19 +22,26 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:caption, :image)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
