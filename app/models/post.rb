db13e765cb351c2bc308d54b4ecdc8e2147498b1
rkugot <stream13k@gmail.com>
2017-07-26 17:09:17 +0300

    Add bootstrap and styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :image, presence: true
-  has_attached_file :image, styles: { medium: "300x300>" }
+  has_attached_file :image, styles: { medium: "640!" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
