ff5ebd9dfc732a4d3bca9b50e22b39fe1e38e9d4
rkugot <stream13k@gmail.com>
2017-07-27 18:55:41 +0300

    Add length of caption validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :image, presence: true
+  validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
   has_attached_file :image, styles: { medium: "640x", small: "340x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :user
