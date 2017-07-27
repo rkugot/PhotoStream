ca4b562e39e7037c45ec52ac3bbe61ae35c4a1d3
rkugot <stream13k@gmail.com>
2017-07-27 18:09:25 +0300

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { medium: "640x", small: "340x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
