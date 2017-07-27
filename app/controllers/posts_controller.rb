14006fdd6208fd993e8a8cca278e8a8957b8077b
rkugot <stream13k@gmail.com>
2017-07-27 15:21:19 +0300

    User can edit and delete only posts created by himself

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :post_owner, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -53,4 +54,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def post_owner
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you"
+      redirect_to root_path
+    end
+  end
 end
