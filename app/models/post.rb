d72a21b01f1cf1ac87e92a0801ef630c41b1acb1
rkugot <stream13k@gmail.com>
2017-07-27 15:01:09 +0300

    Connect user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  validates :user_id, presence: true
   validates :image, presence: true
   has_attached_file :image, styles: { medium: "640x", small: "340x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  belongs_to :user
 end
